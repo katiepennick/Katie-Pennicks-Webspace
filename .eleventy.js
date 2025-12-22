8bce9f3267bae4254fb8b54c195853467657e3ec
katiepennick <250594141+katiepennick@users.noreply.github.com>
2025-12-22 00:43:15 +0000

    Format links, code blocks, and footer. Separate out css files

--- .eleventy.js
+++ .eleventy.js
@@ -8,7 +8,7 @@ module.exports = function (eleventyConfig) {
   });
 
   // Copy these through to the output site unchanged
-  eleventyConfig.addPassthroughCopy("style.css");
+  eleventyConfig.addPassthroughCopy("css");
   eleventyConfig.addPassthroughCopy("images");
   eleventyConfig.addPassthroughCopy("fonts");
   eleventyConfig.addPassthroughCopy("robots.txt");
